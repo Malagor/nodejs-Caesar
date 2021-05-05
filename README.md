62bd7014e6d09419ef232b20a903f1ac2df6d7f6
Aleksandr Pisarik <aleksandr_pisarik@epam.com>
2021-05-05 23:49:59 +0300

    feat: add test for non latin chars and numbers

--- README.md
+++ README.md
@@ -5,4 +5,7 @@
 
 Task 1: https://github.com/rolling-scopes-school/basic-nodejs-2021Q2/blob/master/descriptions/caesar-cipher-cli-tool.md
 
+### Scripts
 
+#### Tests
+> `npm run test`
--- enigma.test.js
+++ enigma.test.js
@@ -3,6 +3,9 @@ const {enigma} = require('./enigma');
 const original = 'This is secret. Message about "_" symbol!';
 const encode7 = 'Aopz pz zljyla. Tlzzhnl hivba "_" zftivs!';
 const encode_1 = 'Sghr hr rdbqds. Ldrrzfd zants "_" rxlank!';
+const nonlatin = 'Это секретный секрет, который не должен зашифроваться.';
+const mixcontent = 'This is secret. А это не латиница. Message about "_123" symbol!';
+const mixcontent7 = 'Aopz pz zljyla. А это не латиница. Tlzzhnl hivba "_123" zftivs!';
 
 test('encode +7', () => {
   expect(enigma('encode', '7', original)).toBe(encode7);
@@ -12,11 +15,18 @@ test('decode +7', () => {
   expect(enigma('decode', '7', encode7)).toBe(original);
 });
 
-
 test('encode -1', () => {
   expect(enigma('encode', '-1', original)).toBe(encode_1);
 });
 
 test('decode -1', () => {
   expect(enigma('decode', '-1', encode_1)).toBe(original);
+});
+
+test('non latin chars', () => {
+  expect(enigma('encode', '7', nonlatin)).toBe(nonlatin);
+});
+
+test('mix latin, non latin and number', () => {
+  expect(enigma('encode', '7', mixcontent)).toBe(mixcontent7);
 });
