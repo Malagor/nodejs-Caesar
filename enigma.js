427d0d7aa2e698247cf8411cbc7cb8cbbd2fb8ab
Aleksandr Pisarik <aleksandr_pisarik@epam.com>
2021-05-06 00:17:50 +0300

    refactor: enigma.js

--- enigma.js
+++ enigma.js
@@ -3,33 +3,43 @@ const ALPHABET_CAPITAL = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 const ALPHABET_LENGTH = ALPHABET.length;
 
 function enigma(action, shift, text) {
+  if (typeof text !== 'string') {
+    throw new Error('Data is not a string');
+  }
+
+  if (isNaN(+shift)) {
+    throw new Error('Shift is not a number');
+  }
+
+  if (action !== 'encode' && action !== 'decode') {
+    throw new Error('Action must be encode/decode');
+  }
+
+  const regexSmallChars = new RegExp(/[a-z]/);
+  const regexLatinChars = new RegExp(/[A-Za-z]/);
+
   let locShift = 0;
 
   if (action === 'encode') {
     locShift = shift < 0 ? ALPHABET_LENGTH + +shift : shift;
+  } else {
+    locShift = shift < 0 ? -shift : ALPHABET_LENGTH - +shift;
   }
 
-  if (action === 'decode') {
-    locShift = shift < 0 ? -shift: ALPHABET_LENGTH - +shift  ;
-  }
+  return text
+    .split('')
+    .map(char => {
+      if (!regexLatinChars.test(char)) {
+        return char;
+      }
 
-  const original = text.split('');
-  const regex = new RegExp(/[a-z]/);
-  const regexCapital = new RegExp(/[A-Z]/);
+      const alphabet = regexSmallChars.test(char) ? ALPHABET : ALPHABET_CAPITAL;
 
-  const encodeStr = original.map(char => {
-    if (!regex.test(char) && !regexCapital.test(char)) {
-      return char;
-    }
+      const newIndex = (alphabet.findIndex(c => c === char) + +locShift) % ALPHABET_LENGTH;
 
-    const alphabet = regex.test(char) ? ALPHABET : ALPHABET_CAPITAL;
-
-    const index = alphabet.findIndex(c => c === char) + +locShift;
-    const newIndex = index % ALPHABET_LENGTH;
-    return alphabet[newIndex];
-  });
-
-  return encodeStr.join('');
+      return alphabet[newIndex];
+    })
+    .join('');
 }
 
 module.exports = {
