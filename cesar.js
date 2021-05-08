72fd5b605fc6ab1fc5bb2c4613908150576ba021
Aleksandr Pisarik <aleksandr_pisarik@epam.com>
2021-05-08 20:57:59 +0300

    feat: app works with pipe

--- cesar.js
+++ cesar.js
@@ -6,36 +6,13 @@ const {transformData} = require('./transformData');
 const {shift, action, output, input} = getParams();
 
 if (!checkParams(shift, action)) {
+  process.stderr.write('Missing or incorrect parameters --shift or --action');
   process.exit(-1)
 }
 
-transformData(action, shift, ...createStreams(input, output));
+const {inputStream, outputStream} = createStreams(input, output);
+const transformStream = transformData(action, shift);
 
-
-//
-// process.stdin.resume();
-// process.stdin.setEncoding('ascii');
-//
-// let inputString = '';
-// let currentLine = 0;
-//
-// process.stdin.on('data', inputStdin => {
-//   inputString += inputStdin;
-// });
-//
-// process.stdin.on('end', _ => {
-//   inputString = inputString.trim().split('\n').map(string => {
-//     return string.trim();
-//   });
-//
-//   printData();
-// });
-//
-// function readline() {
-//   return inputString[currentLine++];
-// }
-//
-// function printData() {
-//   process.stdout.write("hello: " + inputString);   // without auto '\n' (newline)
-//
-// }
+inputStream
+  .pipe(transformStream)
+  .pipe(outputStream);
