cae5743c4ba3ca354cd3a8d18db6bc1edbc5e133
Aleksandr Pisarik <aleksandr_pisarik@epam.com>
2021-05-09 22:34:04 +0300

    feat: add check for access files for write and read

--- createStreams.js
+++ createStreams.js
@@ -2,16 +2,29 @@ const fs = require('fs');
 
 function createStreams(input, output) {
   // input Stream
-  const inputStream = input
-    ? fs.createReadStream(input)
-    : process.stdin;
+  // const inputStream = input
+  //   ? fs.createReadStream(input)
+  //   : process.stdin;
+
+  let inputStream;
+  if (input) {
+    const hasFile = checkFileExistsSync(input, fs.constants.F_OK | fs.constants.R_OK);
+
+    if (hasFile) {
+      inputStream = fs.createReadStream(input);
+    } else {
+      errorHandler(null, input);
+    }
+  } else {
+    inputStream = process.stdin;
+  }
 
   inputStream.on('error', (err) => errorHandler(err, input));
 
   // output Stream
   let outputStream;
   if (output) {
-    const hasFile = checkFileExistsSync(output);
+    const hasFile = checkFileExistsSync(output, fs.constants.F_OK | fs.constants.W_OK);
 
     if (hasFile) {
       outputStream = fs.createWriteStream(output, {flags: 'a'});
@@ -22,13 +35,16 @@ function createStreams(input, output) {
     outputStream = process.stdout;
   }
 
+  outputStream.on('error', (err) => errorHandler(err, output));
+
   return {inputStream, outputStream};
 }
 
-function checkFileExistsSync(filepath) {
+function checkFileExistsSync(filepath, mode) {
   let flag = true;
   try {
-    fs.accessSync(filepath, fs.constants.F_OK);
+    fs.accessSync(filepath, mode);
+    // fs.accessSync(filepath, fs.constants.F_OK);
   } catch (err) {
     flag = false;
   }
@@ -37,6 +53,9 @@ function checkFileExistsSync(filepath) {
 
 function errorHandler(err, filename) {
   process.stderr.write(`There are problems accessing the file: "${filename}".`);
+  if (err) {
+    process.stderr.write(`\n${err}`);
+  }
   process.exit(-1);
 }
 
