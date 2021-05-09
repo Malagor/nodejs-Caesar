b7011dec85bd1c6eda5af5566a95ca201e9d48ce
Aleksandr Pisarik <aleksandr_pisarik@epam.com>
2021-05-09 23:02:57 +0300

    refactor: new work with commander (params)

--- caesar.js
+++ caesar.js
@@ -1,22 +1,25 @@
+const {pipeline} = require('stream');
 const {getParams} = require('./getParams');
-const {checkParams} = require('./checkParams');
 const {createStreams} = require('./createStreams');
 const {transformData} = require('./transformData');
 
 function caesar() {
   const {shift, action, output, input} = getParams();
-
-  if (!checkParams(shift, action)) {
-    process.stderr.write('Missing or incorrect parameters --shift or --action');
-    process.exit(-1);
-  }
-
   const {inputStream, outputStream} = createStreams(input, output);
   const transformStream = transformData(action, shift);
 
-  inputStream
-    .pipe(transformStream)
-    .pipe(outputStream);
+  pipeline(
+    inputStream,
+    transformStream,
+    outputStream,
+    (err) => {
+      if (err) {
+        process.stderr.write(`Process "${action}" failed.`, err);
+        process.exit(-1);
+      }
+
+    }
+  )
 }
 
 caesar();
--- getParams.js
+++ getParams.js
@@ -1,14 +1,29 @@
-const {Command} = require('commander');
+const {Command, Option, InvalidOptionArgumentError} = require('commander');
+
+function checkShift(value) {
+  if (isNaN(+value)) {
+    throw new InvalidOptionArgumentError('Shift value must be a number.');
+  }
+  return +value;
+}
 
 function getParams() {
   const program = new Command();
 
   program.version('0.0.1');
   program
-    .option('-s, --shift <count>', 'a shift')
-    .option('-a, --action <type>', 'an action encode/decode')
+    .addHelpText('before', 'This utility will encode or decode your input using Caesar ciphering algorithm')
+    .addOption(new Option('-a, --action <encode|decode>', 'an action encode/decode')
+      .choices(['encode', 'decode'])
+      .makeOptionMandatory(true)
+    )
+    .addOption(new Option('-s, --shift <number>', 'A shift of the Caesar cipher')
+      .makeOptionMandatory(true)
+      .argParser(checkShift)
+    )
     .option('-i, --input <filename>', 'an input file')
-    .option('-o, --output <filename>', 'an output file');
+    .option('-o, --output <filename>', 'an output file')
+    .addHelpText('afterAll', '\nCreated by Aleksandr Pisarik (Malagor) for NodeJs course 2021/Q2 at RS-School');
 
   program.parse(process.argv);
 
